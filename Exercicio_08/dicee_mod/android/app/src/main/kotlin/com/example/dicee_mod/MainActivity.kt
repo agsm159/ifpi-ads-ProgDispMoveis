e65125fba20fae4068718854bf7c360de362363c
Antônio Gabriel <61834224+agsm159@users.noreply.github.com>
2022-03-23 09:54:42 -0300

    Delete Exercicio_08 directory

--- Exercicio_08/dicee_mod/android/app/src/main/kotlin/com/example/dicee_mod/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dicee_mod
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Exercicio_08/increment_decrement_mod/android/app/src/main/kotlin/com/example/increment_decrement_mod/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.increment_decrement_mod
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
